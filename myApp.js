a28e3e5dab48fda2c5c3bc5394111edd1a98110d
MsDfL-Kode <motaung16nc@icloud.com>
2024-11-29 03:54:47 +0200

    Use yan and fix double listening error

--- myApp.js
+++ myApp.js
@@ -4,9 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Useful Programmer Info Security App Started On Port ${PORT}`);
-});
+
 
 
 
@@ -60,5 +58,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Useful Programmer Info Security App Started On Port ${port}`);
 });
